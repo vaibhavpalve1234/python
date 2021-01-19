a9fac371cc7abc489226372a97a2b5cf92660193
vaibhav <vaibhavpalve1234@gmail.com>
2021-01-19 22:44:46 +0530

    day1
    Signed-off-by: vaibhav <vaibhavpalve1234@gmail.com>

--- kadanne_algorithm.py
+++ kadanne_algorithm.py
@@ -13,7 +13,28 @@ def solve(arr):
                 sum1+=arr[k]
             max_sum = max(max_sum,sum1)
     print(max_sum)
-    return
+    
+# pre-compitution
+def solve1(arr):
+    max_sum =0
+    presum = [0]*len(arr)
+    for i in range(len(arr)):
+        for j in range(0,i):
+            presum[j] += arr[j]
+    print(presum) 
+    for i in range(0,len(arr)):
+        for j in range(i,len(arr)):
+            sum = presum[j] - presum[i-1]
+            max_sum = max(max_sum,sum)
+    print(max(presum))
+    
+def solve2(arr):
+    curr_sum = arr[0]
+    max_sum = arr[0]
+    for i in range(1,len(arr)):
+        curr_sum = max(curr_sum+arr[i],arr[i])
+        max_sum = max(curr_sum,max_sum)
+    print(max_sum)
 if __name__ == "__main__":
     arr =[2,5,-10,7,8,5]
-    solve(arr)
+    solve2(arr)
